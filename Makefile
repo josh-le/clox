2f93be76507f156d4e10f430b8db1cfc6f68bdb8
Josh LeBlanc <joshcapitals07@gmail.com>
2025-08-19 16:02:56 -0400

    14.5 done, set up a constant array and a debug instruction for printing constants

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-FILES = main.c chunk.c memory.c debug.c
+FILES = main.c chunk.c memory.c debug.c value.c
 
 a:
 	gcc $(FILES) -o clox
--- main.c
+++ main.c
@@ -7,6 +7,10 @@ int main(int argc, const char* argv[]) {
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
 
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, constant);
+ 
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
